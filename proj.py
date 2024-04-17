03916ae1813e64ee41fa2f72964f2327dad92050
gregorzadnik <gregor.zadnik@gmail.com>
2024-04-17 20:35:25 +0200

    Fixed network, added reader

---
+++ proj.py
@@ -0,0 +1,26 @@
+import networkx as nx
+def read(filepath):
+    G = nx.Graph()
+    with open(filepath, 'r') as file:
+        line = file.readline()
+        n = int(line.strip().split(" ")[1])
+        for _ in range(n):
+            parts = file.readline().strip().split("!")
+            id = int(parts[0])
+            name = parts[1]
+            label = parts[2].strip()
+            G.add_node(id, name=name, label=label)
+        line = file.readline()
+        m = int(line.strip().split(" ")[1])
+        for _ in range(m):
+            parts = file.readline().strip().split(" ")
+            G.add_edge(int(parts[0]), int(parts[1]))
+    return G
+
+
+filepath = "works_labeled.net"
+G = read(filepath)
+n = G.number_of_nodes()
+m = G.number_of_edges()
+print(n)
+print(m)
